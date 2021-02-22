a2086f7849c4940203332af352e9aeef21716c44
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-02-22 15:56:46 +0000

    Dummy commit.

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/AppBar.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/AppBar.kt
@@ -22,4 +22,6 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.mate
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.Co
