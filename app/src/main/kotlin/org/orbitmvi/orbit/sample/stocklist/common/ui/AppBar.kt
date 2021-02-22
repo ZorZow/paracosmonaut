9b58cded6da0019f2300388f35793b291a317c85
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-02-22 16:10:07 +0000

    New translations messages.pot (Thai)

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/AppBar.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/AppBar.kt
@@ -24,4 +24,7 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.Co
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import org.orbitmvi.orbit.sa
