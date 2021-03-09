afacedbcce89e670dea17f94d1bd7316b998025b
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-03-09 20:51:44 +0000

    Added a comment [skip CI]

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/PriceBox.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/PriceBox.kt
@@ -24,3 +24,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Ali
