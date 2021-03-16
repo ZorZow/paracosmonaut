d9a9a9f82de883609067bcbc14e03c59e5b2b670
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-03-16 21:51:37 +0000

    set up

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/PriceBox.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/PriceBox.kt
@@ -31,4 +31,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
-import androidx.compos
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Fon
