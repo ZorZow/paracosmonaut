8b60075e4a3b2339c86cdfd5516561886d7c94a8
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-05-09 22:09:52 +0000

    тест

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
@@ -10,4 +10,10 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permis
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.orbitmvi.orbit.sample.stocklist.list.ui
+
+import androidx.compos
