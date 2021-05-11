1056eec670a478a1915eebaf461483ff8247ff01
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-05-11 10:39:15 +0000

    * Cleanup.

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
@@ -19,4 +19,6 @@ package org.orbitmvi.orbit.sample.stocklist.list.ui
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fil
