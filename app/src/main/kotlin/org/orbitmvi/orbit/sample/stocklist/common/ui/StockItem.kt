18d17089f0fc9377952979fd9d8d4a521d3a0c1d
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-05-13 09:00:32 +0000

    Back to development version .

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/common/ui/StockItem.kt
@@ -28,4 +28,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compos
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.co
