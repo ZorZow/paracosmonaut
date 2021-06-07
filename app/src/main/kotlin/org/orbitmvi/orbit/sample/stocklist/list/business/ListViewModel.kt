20db1569ac29ede542eb76a036d5331c3658ce67
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-06-07 13:03:19 +0000

    popravek

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/business/ListViewModel.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/business/ListViewModel.kt
@@ -1,4 +1,6 @@
 /*
  * Copyright 2021-2022 Mikołaj Leszczyński & Appmattus Limited
  *
- * Licensed under the Apache License, Version 2.0 (the "
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a cop
