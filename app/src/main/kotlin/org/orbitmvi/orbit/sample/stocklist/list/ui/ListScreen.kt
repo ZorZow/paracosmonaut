cfc66bb68f1e21cfea0d0c784c60c869134f60ce
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-06-16 20:11:45 +0000

    Translated using Translate.Akeeba (French)

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/ui/ListScreen.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/ui/ListScreen.kt
@@ -1,4 +1,6 @@
 /*
  * Copyright 2021-2022 Mikołaj Leszczyński & Appmattus Limited
  *
- * Licensed under the Apache License, Version 2.0
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ 
