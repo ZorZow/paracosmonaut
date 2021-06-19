9f8b3aba22457008d9eaf65b70d92b96bf832ac1
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-06-19 14:23:29 +0000

    Minor clean-ups.

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/ui/ListScreen.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/list/ui/ListScreen.kt
@@ -21,4 +21,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavContro
+import androidx.navigation.NavController
+import org.orbitmvi.orbit.compose.collectAsState
+import org.orbitmvi.orbit.compose.
