2f71d16c87a51737f7759006fe07485ef9d6669c
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-07-03 12:18:44 +0000

    ew

--- app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/streaming/stock/StockDetail.kt
+++ app/src/main/kotlin/org/orbitmvi/orbit/sample/stocklist/streaming/stock/StockDetail.kt
@@ -1,4 +1,5 @@
 /*
  * Copyright 2021 Mikołaj Leszczyński & Appmattus Limited
  *
- * Licensed under the Apache Lice
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the L
