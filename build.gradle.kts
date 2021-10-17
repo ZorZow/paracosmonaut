4793140ce92bc3ee91fa18b4a01e7468c960e279
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-10-17 19:04:18 +0000

    Fixed German translation by Jens Seidel <jensseidel@users.sf.net>.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,5 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.ko
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+        classpath("androidx.navigation:navigation-
