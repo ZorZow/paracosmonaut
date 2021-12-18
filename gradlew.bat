3caf95a072b2dffa3d479d5b02cd1bb5859d719b
ZorZow <109072549+ZorZow@users.noreply.github.com>
2021-12-18 21:50:15 +0000

    minor readme update

--- gradlew.bat
+++ gradlew.bat
@@ -1,4 +1,6 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (t
+@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem you may not use this file except in compliance with the License.
+@re
