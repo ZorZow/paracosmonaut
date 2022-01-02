e2d6ab5aaa917d4b704436a65fa33b4b587e7b4b
ZorZow <109072549+ZorZow@users.noreply.github.com>
2022-01-02 14:23:54 +0000

    added pdf

--- gradlew.bat
+++ gradlew.bat
@@ -21,4 +21,5 @@
 @rem
 @rem ##########################################################################
 
-@rem S
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT"
