95dd423e5bd1420db0f29e282f79d2e55a7f1f3e
ZorZow <109072549+ZorZow@users.noreply.github.com>
2022-01-04 12:03:36 +0000

    Imported Translations from Zanata

--- gradlew.bat
+++ gradlew.bat
@@ -27,4 +27,7 @@ if "%OS%"=="Windows_NT" setlocal
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "." and ".." in APP_HOME to make it shorter.
+for %%i in ("%APP_HO
