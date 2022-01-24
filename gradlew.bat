4face247b5005d32aeabd7e1f65a22bf2b2f0515
ZorZow <109072549+ZorZow@users.noreply.github.com>
2022-01-24 15:40:18 +0000

    [l10n] Updated Italian translation.

--- gradlew.bat
+++ gradlew.bat
@@ -46,4 +46,10 @@ echo.
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
-echo location of your Jav
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAV
