fdfcbe3ccc5b25c57e47f418dca7f92bde199f3b
ZorZow <109072549+ZorZow@users.noreply.github.com>
2022-03-19 13:32:16 +0000

    add node version

--- gradlew.bat
+++ gradlew.bat
@@ -81,4 +81,9 @@ if "%ERRORLEVEL%"=="0" goto mainEnd
 rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
